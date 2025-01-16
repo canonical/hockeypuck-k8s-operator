057d50e8cb18890c37949ec26159a8e6ed002665
Swetha Swaminathan <swetha.swaminathan@canonical.com>
2025-01-16 16:28:39 +0530

    CI failures

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -23,16 +23,16 @@ async def test_build_and_deploy(ops_test: OpsTest, pytestconfig: pytest.Config):
     Assert on the unit status before any relations/configurations take place.
     """
     # Deploy the charm and wait for active/idle status
-    METADATA = yaml.safe_load(Path("./metadata.yaml").read_text(encoding="utf-8"))
-    APP_NAME = METADATA["name"]
+    metadata = yaml.safe_load(Path("./metadata.yaml").read_text(encoding="utf-8"))
+    app_name = metadata["name"]
     charm = pytestconfig.getoption("--charm-file")
-    resources = {"httpbin-image": METADATA["resources"]["httpbin-image"]["upstream-source"]}
+    resources = {"httpbin-image": metadata["resources"]["httpbin-image"]["upstream-source"]}
     assert ops_test.model
     await asyncio.gather(
         ops_test.model.deploy(
-            f"./{charm}", resources=resources, application_name=APP_NAME, series="jammy"
+            f"./{charm}", resources=resources, application_name=app_name, series="jammy"
         ),
         ops_test.model.wait_for_idle(
-            apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
+            apps=[app_name], status="active", raise_on_blocked=True, timeout=1000
         ),
     )
