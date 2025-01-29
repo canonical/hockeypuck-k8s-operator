38c6d87a5d0fd1a2096fa46c21274581b8d3152c
Swetha Swaminathan <swetha.swaminathan@canonical.com>
2025-01-29 21:52:44 +0530

    Fix linting issues

--- lib/charms/grafana_k8s/v0/grafana_dashboard.py
+++ lib/charms/grafana_k8s/v0/grafana_dashboard.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Canonical Ltd.
+# Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
 """## Overview.
@@ -189,6 +189,7 @@ from pathlib import Path
 from typing import Any, Callable, Dict, List, Optional, Tuple
 
 import yaml
+from cosl import DashboardPath40UID, LZMABase64
 from ops.charm import (
     CharmBase,
     HookEvent,
@@ -208,7 +209,6 @@ from ops.framework import (
     StoredState,
 )
 from ops.model import Relation
-from cosl import LZMABase64, DashboardPath40UID
 
 # The unique Charmhub library identifier, never change it
 LIBID = "c49eb9c7dfef40c7b6235ebd67010a3f"
--- lib/charms/prometheus_k8s/v0/prometheus_scrape.py
+++ lib/charms/prometheus_k8s/v0/prometheus_scrape.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Canonical Ltd.
+# Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 """Prometheus Scrape Library.
 
