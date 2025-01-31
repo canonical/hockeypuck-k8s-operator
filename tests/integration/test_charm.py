f11d9ab6af9880c28027e39333ec88a4fa67ae28
Swetha Swaminathan <swetha.swaminathan@canonical.com>
2025-01-31 17:06:35 +0530

    Fix linting issues

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -19,13 +19,9 @@ async def test_hockeypuck_health(hockeypuck_k8s_app: Application) -> None:
     act: Do a get request to the main page.
     assert: Returns 200 and the page contains the correct data.
     """
-    status = await hockeypuck_k8s_app.model.get_status()
-    unit_ips = [
-        unit.address for unit in status.applications[hockeypuck_k8s_app.name].units.values()
-    ]
-    for unit_ip in unit_ips:
+    for unit in hockeypuck_k8s_app.units:
 
-        url = f"http://{unit_ip}:11371"
+        url = f"http://{unit.address}:11371"
         res = requests.get(
             url,
             timeout=120,
