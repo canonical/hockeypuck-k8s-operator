d1983c9873976191775c08563860cca65eb893fa
Swetha Swaminathan <swetha.swaminathan@canonical.com>
2025-02-13 11:03:15 +0530

    move command execution code

--- src/actions.py
+++ src/actions.py
@@ -4,6 +4,7 @@
 """Hockeypuck charm actions."""
 
 import logging
+from typing import Any, Dict
 
 import ops
 import paas_app_charmer.go
@@ -33,13 +34,42 @@ class Observer(ops.Object):
         )
 
     def _blacklist_and_delete_key_action(self, event: ops.ActionEvent) -> None:
-        """Blacklist and delete a key/s from the database.
+        """Blacklist and delete keys from the database.
 
         Args:
             event: the event triggering the original action.
         """
         fingerprints = event.params["fingerprints"]
         ticket_id = event.params["ticket-id"]
+        env = {"TICKET_ID": ticket_id, "APP_DELETE_FINGERPRINTS": fingerprints}
+        command = [
+            "/hockeypuck/bin/delete_keys",
+        ]
+        self._execute_action(event, command, env)
+
+    def _rebuild_prefix_tree_action(self, event: ops.ActionEvent) -> None:
+        """Rebuild the prefix tree using the hockeypuck-pbuild binary.
+
+        Args:
+            event: the event triggering the original action.
+        """
+        command = [
+            "/hockeypuck/bin/hockeypuck-pbuild",
+            "-config",
+            "/hockeypuck/etc/hockeypuck.conf",
+        ]
+        self._execute_action(event, command)
+
+    def _execute_action(
+        self, event: ops.ActionEvent, command: str, env: Dict[str, Any] = None
+    ) -> None:
+        """Execute the action.
+
+        Args:
+            event: the event triggering the original action.
+            command: the command to be executed inside the hockeypuck container.
+            env (optional): Any extra environment variables to be passed to the command.
+        """
         if not self.charm.is_ready():
             event.fail("Service not yet ready.")
         hockeypuck_container = self.charm.unit.containers[HOCKEYPUCK_CONTAINER_NAME]
@@ -48,41 +78,11 @@ class Observer(ops.Object):
 
             _ = hockeypuck_container.pebble.stop_services(services=[service_name])
             environment = self.charm._gen_environment()
-            environment.update({"TICKET_ID": ticket_id, "APP_DELETE_FINGERPRINTS": fingerprints})
-            command = [
-                "/hockeypuck/bin/delete_keys",
-            ]
+            if env is not None:
+                environment.update(env)
             process = hockeypuck_container.exec(
                 command,
                 environment=environment,
-            )
-            _, _ = process.wait_output()
-        except ops.pebble.ExecError as ex:
-            logger.exception("Action %s failed: %s %s", ex.command, ex.stdout, ex.stderr)
-            event.fail(f"Failed: {ex.stderr!r}")
-        finally:
-            _ = hockeypuck_container.pebble.start_services(services=[service_name])
-
-    def _rebuild_prefix_tree_action(self, event: ops.ActionEvent) -> None:
-        """Rebuild the prefix tree using the hockeypuck-pbuild binary.
-
-        Args:
-            event: the event triggering the original action.
-        """
-        if not self.charm.is_ready():
-            event.fail("Service not yet ready.")
-        hockeypuck_container = self.charm.unit.containers[HOCKEYPUCK_CONTAINER_NAME]
-        service_name = next(iter(self.charm._container.get_services()))
-        try:
-            _ = hockeypuck_container.pebble.stop_services(services=[service_name])
-            command = [
-                "/hockeypuck/bin/hockeypuck-pbuild",
-                "-config",
-                "/hockeypuck/etc/hockeypuck.conf",
-            ]
-            process = hockeypuck_container.exec(
-                command,
-                environment=self.charm._gen_environment(),
             )
             _, _ = process.wait_output()
         except ops.pebble.ExecError as ex:
