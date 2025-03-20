f692735098cb9c4accf3f39f7d06821f3da9b833
swetha1654 <swetha.swaminathan@canonical.com>
2025-03-20 14:30:53 +0530

    remove providers

--- terraform/product/providers.tf
+++
@@ -1,7 +0,0 @@
-provider "juju" {
-  alias          	= "opencti_db"
-  controller_addresses = "juju-controller-36-staging-ps6.dynamic.admin.canonical.com:17070"
-  ca_certificate   	= data.vault_generic_secret.juju_controller_certificate.data["ca_cert"]
-  username        	= data.vault_generic_secret.juju_credentials_stg_opencti_db.data["username"]
-  password        	= data.vault_generic_secret.juju_credentials_stg_opencti_db.data["password"]
-}
