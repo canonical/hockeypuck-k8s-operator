460a308d9bd2544696dd6bf9d55922e1d878b277
swetha1654 <swetha.swaminathan@canonical.com>
2025-04-28 11:40:25 +0530

    WIP

---
+++ tests/unit/test_scenario.py
@@ -0,0 +1,13 @@
+from ops import testing
+
+from src.charm import HockeypuckK8SCharm
+
+
+def test_base():
+    ctx = testing.Context(HockeypuckK8SCharm)
+    relation1 = testing.Relation("postgresql")
+    state_in = testing.State(planned_units=2, relations=[relation1])
+    state_out = ctx.run(ctx.on.start(), state_in)
+    assert state_out.unit_status == testing.BlockedStatus(
+        "Hockeypuck does not support multi-unit deployments"
+    )
